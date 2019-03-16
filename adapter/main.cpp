e73fc1b181e95b295131098bf1341e67ad99756c
Cihangir Zengin <cihangirzengin@gmail.com>
2019-03-16 12:06:48 +0200

    multiple source and project directory is added

--- adapter/main.cpp
+++ adapter/main.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 #include <sstream>
+#include <ostream>
 
 class String{
 
     std::string s;
-    public:
+public:
     String( const std::string& s ):s( s ) {
 
     } 
@@ -15,7 +16,7 @@ class String{
         return { boost::to_lower_copy(s)} ; 
     }
 
-    vector<String> split( std::string delim=" ") {
+    std::vector<String> split( std::string delim=" ") {
         std::vector<std::string> parts;
 
         boost::split( parts , s , boost::is_any_of( delim ) , boost::token_compress_on );
@@ -28,12 +29,22 @@ class String{
         return s.length();
     }
 
+    friend std::ostream& operator<<(std::ostream& os , const String& p ){
+        os << p.s ;
+        return os;
+    }
 };
 
 int main( int argc , char* argv[] ) {
 
-    String s{"Benim Adim Kamil"};
+    String s{"I Am A Big String"};
 
+    std::cout << s << std::endl ;
+    std::cout << s.to_lower_copy() << std::endl ;
+    
+    for(auto& w : s.split() ) {
+        std::cout << w << " "<<  w.length() << std::endl ;
+    }
     
     
   
